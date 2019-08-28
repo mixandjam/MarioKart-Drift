5e936d6234cb246924a188fb75acd6a9dddb5433
Zachary Sheppard <the_ds_guy@yahoo.com>
2019-08-28 00:44:11 -0500

    Adding AI to the mariokart prototype

---
+++ Assets/PlayerInputProvider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerInputProvider : MonoBehaviour
+{
+
+    [SerializeField]
+    private KartController kart = null;
+
+    private void Update()
+    {
+        if (kart == null)
+        {
+            return;
+        }
+
+        // ZAS: If we are accelerating, tell the kart to accelerate
+        if (Input.GetButton("Fire1"))
+            kart.Accelerate();
+
+        if (Input.GetButton("Jump"))
+            kart.Jump();
+
+        // ZAS: Tell the kart how to steer each update
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        kart.Steer(horizontalMovement);
+    }
+
+}
---
+++ Assets/TrackData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class TrackData : ScriptableObject
+{
+    [SerializeField]
+    private Vector3[] m_trackNodes = null;
+
+    public int GetNodeLength()
+    {
+        return m_trackNodes.Length;
+    }
+
+    public Vector3? GetTargetNode(int index)
+    {
+        if (index < 0 || index >= m_trackNodes.Length)
+        {
+            return null;
+        }
+
+        return m_trackNodes[index];
+    }
+
+    public void SetPoints(Vector3[] poits)
+    {
+        m_trackNodes = poits;
+    }
+}
